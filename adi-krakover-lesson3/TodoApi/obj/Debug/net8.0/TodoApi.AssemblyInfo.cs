962c8e7848209a67bb6c6353212821c14cee89ce
adikrakover <ak0556745911@gmail.com>
2025-11-29 21:18:04 +0200

    API: Added Dockerfile for deployment on Render

--- adi-krakover-lesson3/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ adi-krakover-lesson3/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70fd3a6f344db905d2f87e8d9da4337113398e4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4452e3ea0031b089f9d07752c289b50c5ed7f984")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
