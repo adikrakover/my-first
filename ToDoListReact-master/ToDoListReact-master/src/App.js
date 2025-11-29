4452e3ea0031b089f9d07752c289b50c5ed7f984
adikrakover <ak0556745911@gmail.com>
2025-11-29 19:25:43 +0200

    Design update: Added new modern CSS styling to the React client.

--- ToDoListReact-master/ToDoListReact-master/src/App.js
+++ ToDoListReact-master/ToDoListReact-master/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getTasks, createTask, updateTask, deleteTask } from './service.js';
+import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
